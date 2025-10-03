07cc152cd8a9e5de7abe376202b0ecf117d732f7
Lucia <ds4229@k12.sd.us>
2025-10-03 14:50:46 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,47 @@
+5print("Tally Hall")
+Question1 = input("When was Tally Hall formed?")
+if Question1 == "2002":
+  print("Correct!")
+else:
+  print("Incorrect!")
+Question9 = input("Where was Tally Hall formed?")
+if Question9 == "Ann Arbor Michigan":
+  print("Correct!")
+else:
+  print("Incorrect!")
+Question2 = input("What was the first actual Tally Hall album?")
+if Question2 == "Marvin's Marvelous Mechanical Museum":
+  print("Correct!")
+else:
+  print("Incorrect!")
+Question3 = input("What year did Marvin's release?")
+if Question3 == "2005":
+  print("Correct!")
+else:
+  print("Incorrect!")
+Question4 = input("When did Marvin's rerelease?")
+if Question4 == "2007":
+  print("Correct!")
+else:
+  print("Incorrect!")
+Question5 = input("When did Marvin's re-rerelease?")
+if Question5 == "2008":
+  print("Correct!")
+else:
+  print("Incorrect!")
+Question6 = input("What Tally Hall song got an award?")
+if Question6 == "Good Day":
+  print("Correct!")
+else:
+  print("Incorrect!")
+Question7 = input("Who wrote the award winning song?")
+if Question7 == "Andrew Horowitz":
+  print("Correct!")
+else:
+  print("Incorrect!")
+Question8 = input("What was Tally Hall's second album?")
+if Question8 == "Good & Evil":
+  print("Correct!")
+else:
+  print("Incorrect!")
+Question10 = input("")
